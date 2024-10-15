72bf8b5b67bd32603b060948c46664f1e8b9dbd4
frostywru <garciamatt623@gmail.com>
2024-10-15 14:48:36 +0800

    final counter

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div style="text-align: center;">
+    
+<h1 class="text-5xl">{{ $counter }}</h1>
+<br>
+    <button wire:click="decrement" type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">-</button></button>
+    <button wire:click="increment" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">+</button>
+
+</div>
